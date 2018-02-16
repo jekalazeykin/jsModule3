16cebb6b04f37f0f12452f4fcc1df5eb633c7765
E.Lazeykin <lazejjkin@i.ua>
2018-02-16 08:29:27 +0200

    module-4

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+const alphabet = [
+  ["q","w","e","r","t","y","u","i","o","p","[","]"],
+  ["a","s","d","f","g","h","j","k","l",";","'"],
+  ["z","x","c","v","b","n","m",",",".","/"]
+];
+
+console.log(`${alphabet[1][5]}${alphabet[0][2]}${alphabet[1][8]}${alphabet[1][8]}${alphabet[0][8]}`);
+console.log(`${alphabet[1][6]}${alphabet[1][0]}${alphabet[2][3]}${alphabet[1][0]}${alphabet[1][1]}${alphabet[2][2]}${alphabet[0][3]}${alphabet[0][7]}${alphabet[0][9]}${alphabet[0][4]}`);
+console.log(`${alphabet[0][4]}${alphabet[0][3]}${alphabet[1][0]}${alphabet[0][7]}${alphabet[2][5]}${alphabet[0][2]}${alphabet[0][3]}`);
